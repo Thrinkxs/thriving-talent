28cc806e9dd8b7deee506e40b4ac744d417fc5b4
Sewo Goodness <sewogoodness111@gmail.com>
2025-10-23 00:10:45 +0100

    chore: Update package dependencies to use caret (^) versioning and refactor dashboard components for improved structure and navigation added support for ecruiter-dashboard and related files setup

---
+++ frontend/app/(recruiter_dashboard)/recruiter/post-job/page.tsx
@@ -0,0 +1,6 @@
+import PostJobOverview from "@/modules/recruiter_dashboard/components/post-job/overview";
+import React from "react";
+
+export default function PostJobPage() {
+  return <PostJobOverview />;
+}
---
+++ frontend/modules/recruiter_dashboard/components/post-job/overview.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostJobOverview() {
+  return <div>PostJobOverview</div>;
+}
