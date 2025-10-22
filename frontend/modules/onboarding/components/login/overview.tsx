0ec24c4d48a3d659fc202e2165dfcf7020d34acc
Sewo Goodness <sewogoodness111@gmail.com>
2025-10-22 22:26:07 +0100

    refactor: Remove UserDashboardRootLayout component and update login overview to use next/navigation

--- frontend/modules/onboarding/components/login/overview.tsx
+++ frontend/modules/onboarding/components/login/overview.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useAuth } from "@/contexts/auth-context";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { userRoleEnum } from "@/layout/utils/enum";
 
 export default function LoginPageOverview() {
